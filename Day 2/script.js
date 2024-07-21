92ba21780ed71da485d82c07c803d2e7fd771be6
Shubham Sharma <ss97160@gmail.com>
2024-07-21 23:34:00 +0530

    Day 2 challenge completed

---
+++ Day 2/script.js
@@ -0,0 +1,123 @@
+// Day 2: Operators
+
+// Tasks/Activities:
+
+// Activity 1: Arithmetic Operations
+// • Task 1: Write a program to add two numbers and log the result to the console.
+
+let num1=10
+let num2=20
+let sumOfTwo=num1+num2
+console.log(sumOfTwo)
+
+// • Task 2: Write a program to subtract two numbers and log the result to the console.
+
+let num3=30
+let num4=40
+let subtractOfTwo=num4-num3
+console.log(subtractOfTwo)
+
+
+// • Task 3: Write a program to multiply two numbers and log the result to the console.
+
+let num5=5
+let num6=10
+let productOfTwo=num5*num6
+console.log(productOfTwo)
+
+// • Task 4: Write a program to divide two numbers and log the result to the console.
+let num7=10
+let num8=3
+let divideOfTwo=num7/num8
+console.log(divideOfTwo)
+
+
+// • Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+let num9=10
+let num10=3
+let remainderOfTwo=num9%num10
+console.log(remainderOfTwo)
+
+// Activity 2: Assignment Operators
+// • Task 6: Use the += operator to add a number to a variable and log the result to the console.
+
+let val=10
+val+=20
+console.log(val)
+
+// • Task 7: Use the -= operator to subtract a number from a variable and log the result to the console.
+
+let val2=10
+val2-=20
+console.log(val2)
+
+// Activity 3: Comparison Operators
+// • Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+
+let val3=30
+let val4=40
+console.log(val4>val3)
+console.log(val4<val3)
+
+// • Task 9: Write a program to compare two numbers using > and <= and log the result to the console.
+
+let val5=50
+let val6=60
+console.log(val5<=val6)
+console.log(val5>=val6)
+
+// • Task 10: Write a program to compare two numbers using == and === and log the result to the console.
+
+let val7=70
+let val8=80
+console.log(val7 == val8)
+
+let val9=90
+let val10=90
+console.log(val9 == val10)
+
+let val11="11"
+let val12=11
+console.log(val11 === val12)
+
+let val13=12
+let val14=12
+console.log(val13 === val14)
+
+// Activity 4: Logical Operators
+// • Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
+
+let val15=15
+let val16=16
+console.log(15<16 && 16>0)
+// && gives true only when both conditions true
+
+// • Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
+
+let val17=17
+let val18=18
+console.log(17>18 || 17>10)
+// || gives true if any one condition true
+
+// • Task 13: Write a program that uses the ! operator to negate a condition and log the result to the console.
+
+console.log(!false)
+console.log(!true)
+console.log(12!=="12")
+console.log(11!=12)
+//not equal !=
+//not equal value or not equal type  !==
+
+// Activity 5: Ternary Operator
+// • Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+
+let number=19
+console.log(number>=0?"positive":"negative")
+
+// Achievement:
+// By the end of these activities, students will:
+// • Understand and use arithmetic operators to perform basic calculations.
+// • Use assignment operators to modify variable values.
+// • Compare values using comparison operators.
+// • Combine conditions using logical operators.
+// Use the ternary operator for concise conditional expressions.
